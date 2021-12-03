2f494c6bb4ac246fc8256cb3f480f2dd6af665bc
jmquilez <81701724+jmquilez@users.noreply.github.com>
2021-12-03 09:32:09 +0100

    Add files via upload

---
+++ _1st_practice/_1st_practice.ino
@@ -0,0 +1,19 @@
+//timing interval definition (unused)
+#define timing 1000
+//led pin
+const int led = 13;
+//time delay (subject to increase or decrease)
+int time = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(time);
+  digitalWrite(led, LOW);
+  delay(time);
+  time+=10;
+}
+
