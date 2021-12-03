6fa58bb5014cc5e7989070371096b4a319b4e678
jmquilez <81701724+jmquilez@users.noreply.github.com>
2021-12-03 09:20:05 +0100

    Add files via upload

---
+++ blink/JUSEP.ino
@@ -0,0 +1,19 @@
+
+int ledPin= 10; // elegir Pin para el led
+int botonPin= 5; // elegir pin para botón
+int estadoBoton= 0; // botón apagado
+void setup() {
+pinMode(ledPin, OUTPUT); // declarar Led como salida
+pinMode(botonPin, INPUT); // declarar botón como entrada
+}
+void loop(){
+estadoBoton= digitalRead(botonPin); // leer valor de entrada
+if(estadoBoton== HIGH) { // chequear si el valor leído es “1” (botón presionado)
+digitalWrite(ledPin, LOW); // Led apagado
+} else{//sino
+digitalWrite(ledPin, LOW); // hacer que el Led parpadee
+delay(200);
+digitalWrite(ledPin, HIGH);
+delay(200);
+}
+}
