cce2856750500d4d80657d4b1f555d8403b1d98e
jmquilez <81701724+jmquilez@users.noreply.github.com>
2021-12-03 09:51:29 +0100

    Add files via upload

---
+++ _5th_practice/_5th_practice.ino
@@ -0,0 +1,29 @@
+int ldrPin = 4; 
+int ledPin = 2; 
+int lightVal = 0; 
+ 
+void setup() { 
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  
+} 
+ 
+void loop() { 
+ 
+  lightVal = analogRead(ldrPin);   
+  digitalWrite(ledPin, HIGH); 
+  delay(lightVal);                  
+  digitalWrite(ledPin, LOW);   
+  delay(lightVal);  
+ 
+  Serial.println(lightVal);
+ 
+    if(lightVal<=250){
+  Serial.println("Hágase la luz");
+}else if(lightVal>600){
+    Serial.println("Te pasaste de lanza");
+  }else{
+    Serial.println("No problem");
+  }
+ 
+}
+
